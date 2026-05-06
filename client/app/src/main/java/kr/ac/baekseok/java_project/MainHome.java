8d9424f7fa51c0a9b7cc254890be81eda4d108a6
1dasan <jacob0924.park@gmail.com>
2026-05-06 14:00:22 +0900

    1

---
+++ client/app/src/main/java/kr/ac/baekseok/java_project/MainHome.java
@@ -0,0 +1,15 @@
+package kr.ac.baekseok.java_project;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainHome extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main_home);
+
+
+    }
+}
