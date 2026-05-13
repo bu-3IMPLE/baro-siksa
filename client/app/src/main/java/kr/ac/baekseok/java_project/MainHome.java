5292f4c3776f64ca559e586853be13eea4a75cfb
1dasan <jacob0924.park@gmail.com>
2026-05-13 13:43:35 +0900

    기본 ui디자인

--- client/app/src/main/java/kr/ac/baekseok/java_project/MainHome.java
+++ client/app/src/main/java/kr/ac/baekseok/java_project/MainHome.java
@@ -1,14 +1,40 @@
 package kr.ac.baekseok.java_project;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
+
 public class MainHome extends AppCompatActivity {
+
+    TextView tvMore;
+    NavigationBarView bottomNav;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main_home);
+
+        tvMore=(TextView)findViewById(R.id.tvMore);
+        bottomNav=(NavigationBarView)findViewById(R.id.bottomNav);
+
+
+        //오늘 동네 맛집 더보기
+        tvMore.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        //bottomNav.setOnItemSelectedListener();
 
 
     }
